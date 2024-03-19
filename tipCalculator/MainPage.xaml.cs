afeb88bd8b43e19f6136696b577d3958bf622e28
JulianPaulo <juliandamfirst@gmail.com>
2024-03-19 12:39:05 -0300

    Inicio da Criação de projeto de Calculadora: Criação do Layout

---
+++ tipCalculator/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace tipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PercentageSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            TipPercentageLabel.Text = $"{PercentageSlider.Value.ToString()}%";
+        }
+
+        private void Button15Percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button30Percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundDown_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+}
